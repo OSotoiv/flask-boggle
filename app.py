3964660b6aadffbbb78d403e1df7e1bc9a6608e5
OctavianoSoto <osoto.iv@gmail.com>
2022-10-22 22:47:20 -0500

    not perfect but its working, I need to move on to writing Test now

--- app.py
+++ app.py
@@ -1,32 +1,34 @@
 from crypt import methods
-import json
 from boggle import Boggle
-from flask import Flask, request, render_template, redirect, flash, session, jsonify
+from flask import Flask, request, render_template, redirect, session, jsonify
+from flask_debugtoolbar import DebugToolbarExtension
+
 
 app = Flask(__name__)
 app.debug = True
 app.config['SECRET_KEY'] = 'FLASK-boggle'
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+toolbar_debug = DebugToolbarExtension(app)
 
 boggle_game = Boggle()
 
 
 @app.route('/')
-def base():
-    return render_template('base.html')
-
-
-@app.route('/home')
 def home():
-    return render_template('home.html')
-
-
-@app.route('/start')
-def start():
     board = boggle_game.make_board()
+    if not session.get('active_game'):
+        session['active_game'] = True
+        session['high_score'] = 0
     session['gameboard'] = board
     session['correct'] = []
-    # flash('New Game!', "success")
-    return render_template('start.html', board=board)
+    return render_template('game_on.html', board=board)
+
+
+# @app.route('/active_game')
+# def active_game():
+#     board = session['gameboard']
+#     correct_list = session['correct']
+#     return render_template('game_on.html', board=board, correct_list=correct_list)
 
 
 @app.route('/about')
@@ -48,3 +50,18 @@ def guess():
         session['correct'] = correct_list
         print(correct_list)
     return jsonify({'result': res})
+
+
+@app.route('/dictionary')
+def dictionary():
+    return render_template('dictionary.html')
+
+
+@app.route('/record_score', methods=["POST"])
+def record_score():
+    current_score = len(session['correct'])
+    high_score = session['high_score']
+    if int(current_score) > int(high_score):
+        session['high_score'] = current_score
+        return jsonify({'high_score': int(current_score)})
+    return jsonify({'high_score': 0})
--- static/app.js
+++ static/app.js
@@ -1,10 +1,14 @@
 baseurl = 'http://127.0.0.1:5000'
-const score = $('#score')
-const time = $('#time')
-const ul_list = $('#list')
-const guess_input = $('#guess')
+const score = $('#score');
+const game_time = $('#time');
+const ul_list = $('#list');
+const guess_input = $('#guess');
+const guess_btn = $('#guess_btn');
+const start_btn = $('#start');
+const reset_btn = $('#reset');
 
 $('#guess_form').on('submit', async function (evt) {
+    // handle users input/guess from the form.
     evt.preventDefault();
     const guess = guess_input.val().toLowerCase();
     guess_input.val('')
@@ -15,15 +19,33 @@ $('#guess_form').on('submit', async function (evt) {
         console.log(e)
     }
 })
+start_btn.on('click', startGame)
+
+function startGame() {
+    // enable buttons and inputs start the timer
+    guess_btn.toggleClass('disabled')
+    guess_input.prop('disabled', false)
+    start_btn.toggleClass('disabled');
+    reset_btn.toggleClass('disabled');
+    startTimer()
+}
+function stopGame() {
+    // disable inputs
+    guess_btn.toggleClass('disabled')
+    guess_input.prop('disabled', true)
+}
 
 function updateScore() {
+    // updates the score on the html page
     score.html(parseInt(score.html()) + 1)
 }
 function updateList(word) {
+    // updates ul with valid words
     ul_list.append(`<li>${word}</li>`)
 }
 
-function handle(result, guess) {
+function alertUser(result, guess) {
+    // temporary way to alert the user if word is not valid.
     if (result == 'ok') {
         updateScore()
         updateList(guess)
@@ -37,11 +59,54 @@ function handle(result, guess) {
 }
 
 async function isValidGuess(guess) {
-    const res = await axios({
-        url: `${baseurl}/guess`,
-        method: 'POST',
-        data: { 'guess': guess }
-    });
-    const { result } = res.data;
-    handle(result, guess)
+    // sends word to server to check if its valid
+    try {
+        const res = await axios({
+            url: `${baseurl}/guess`,
+            method: 'POST',
+            data: { 'guess': guess }
+        });
+        const { result } = res.data;
+        alertUser(result, guess)
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+
+let timeoutID;
+let sec = 60
+function startTimer() {
+
+    timeoutID = setInterval(updateTimer, 1000);
+}
+
+function updateTimer() {
+    // update timer on html page also checks if timer reached 0
+    sec = (sec - 1);
+    game_time.html(sec);
+    if (sec == 0) {
+        stopGame();
+        clearTimeout(timeoutID);
+        recordScore()
+    }
+}
+
+async function recordScore() {
+    // send post request to update session score if new high score. 
+    try {
+        const res = await axios({
+            url: `${baseurl}/record_score`,
+            method: 'POST',
+        });
+        const { high_score } = res.data;
+        if (high_score) {
+            alert('new high score!')
+        } else if (high_score == 0) {
+            alert('keep trying')
+        }
+    } catch (e) {
+        alert('something when wrong while recording your score')
+        console.log(e)
+    }
 }
